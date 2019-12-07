541935212e48693c0ca815ab80a577fdf091865e
jianxg <419533848@qq.com>
2019-12-07 14:48:37 +0800

    first code commit

---
+++ src/components/home/Appindex.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      Hello Veu.js
+    </div>
+</template>
+
+<script>
+    export default {
+      name: 'Appindex'
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import AppIndex from '@/components/home/AppIndex'
+import Login from '@/components/Login'
+Vue.use(Router)
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    // 下面都是固定的写法
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/index',
+      name: 'AppIndex',
+      component: AppIndex
+    }
+  ]
+})
